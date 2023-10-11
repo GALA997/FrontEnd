7390d9e15f8ac182db23a135c6a8904f909ad360
Martin <ejmgl99@gmail.com>
2023-10-11 17:00:39 -0600

    Pollito

--- ReactJS-proyects/3-calculadora-interactiva/src/App.js
+++ ReactJS-proyects/3-calculadora-interactiva/src/App.js
@@ -1,9 +1,28 @@
 import './App.css';
+import freeCodeCampLogo from '../src/images/freecodecamp-logo.png'
+import Boton from './components/Boton'
 
 function App() {
   return (
-    <div className="App">
-     
+    <div className='App'>
+        <div className = 'freecodecamp-logo-contenedor'>
+        <img
+            src ={freeCodeCampLogo}
+            className = 'freecodecamp-logo'
+            alt = 'Logo de FreeCodeCamp'
+        />
+        </div>
+
+        <div className = 'contenedor-calculadora'>
+            <div className = 'fila'></div>
+            <Boton>1</Boton>
+            <div className = 'fila'></div>
+            <div className = 'fila'></div>
+            <div className = 'fila'></div>
+
+            <div className = 'fila'></div>
+
+        </div>
     </div>
   );
 }
